8ddc907e65322ec4e4f02d3da38c233f1c069855
JYee Ren <JYee Ren>
2017-02-24 15:11:02 +0800

    first commit

---
+++ routes/signout.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+router.get('/', checkLogin, function (req, res, next) {
+  //清空session信息 登出
+  req.session.user = null;
+
+  req.flash('success', '登出成功');
+  res.redirect('/posts');
+});
+
+module.exports = router;
